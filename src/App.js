4d5d0f43052f030f6121eebaa46bd97abc550686
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-07-18 13:04:32 +0200

    add src

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import user from "./components/Profile/user.json";
+import data from "./components/Statistics/data.json";
+import friends from "./components/FriendList/friends.json";
+import transactions from "./components/TransactionHistory/transactions.json";
+
+export default function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title='Upload stats' stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+      </header>
+    </div>
+  );
+}
